931982b91216573b60df3b394a68435d3ef80f16
Georgie <simmsgeorge7@gmail.com>
2024-08-24 12:29:00 +0100

    mermaid updates

--- mermaid.js
+++ mermaid.js
@@ -2,17 +2,32 @@ mermaid.initialize({
     startOnLoad: true
   });
 
+  // formatting of text like this
+// "stateDiagram-v2\n    [*] --> Still\n    Still --> [*]\n    Still --> Moving\n    Moving --> Still\n    Moving --> Cash\n    Still --> Cash\n    Crash --> [*]"; 
+
   eleM = document.querySelector('.mermaid');
   eleE = document.querySelector('#err');
+
   setTimeout(mermaidDraw, 200);
+
+  // main function called by button 
   async function mermaidDraw() {
+
     try {
-      graphDefinition = await mermaidEval('LocalFile.md');
+        // get text from input
+        text = document.querySelector('textarea').value;
+        console.log(JSON.stringify(text))
+        // check it is a valid graph
+      graphDefinition = await mermaidEval(text);
+
+      //Requests svg of graph and sets it in html
       const {
         svg
       } = await mermaid.render('graphDiv', graphDefinition);
       eleM.innerHTML = svg;
-    } catch (err) {
+
+    } catch (err) { 
+        // if error show errors?
       if (err instanceof ReferenceError) {
         varname = err.message.split(' ')[0];
         window[varname] = varname;
@@ -22,11 +37,9 @@ mermaid.initialize({
       eleE.insertAdjacentHTML('beforeend', `🚫${err.message}\n`);
     }
   };
-  async function mermaidEval(url) {
-    //const response = await fetch(url);
-    //text = await response.text();
-    text = "stateDiagram-v2\n    [*] --> Still\n    Still --> [*]\n    Still --> Moving\n    Moving --> Still\n    Moving --> Cash\n    Still --> Cash\n    Crash --> [*]"; document.querySelector('textarea').value;
-    console.log(JSON.stringify(text))
+  async function mermaidEval(text) {
+
+    // checks its all mermaid
     if (!text.match(/^[a-zA-Z]/)) {
     // markdown ```mermaid, remove first and last line
       text = text.split('\n').slice(1, -1).join('\n');
